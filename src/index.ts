f6fac98228dcc657a15decd186766a377aa70ea8
Luciano Sabino <lb255043@gmail.com>
2025-01-12 16:56:00 -0300

    Atualização para deploy, inicializando as migration automaticamente

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { Knex } from "./server/database/Knex";
 import { app } from "./server/server";
 
-const port = process.env.PORT || 10000;
-
 const startServer = () => {
-  app.listen(port, () => console.log(`Servidor rodando! na porta ${port}`));
+  app.listen(process.env.PORT || 10000, () => {
+    console.log(`App rodando na porta ${process.env.PORT || 10000}`);
+  });
 };
 
-startServer();
+if (process.env.IS_LOCALHOST !== "true") {
+  console.log("Rodando migrations");
+
+  Knex.migrate
+    .latest()
+    .then(() => {
+      Knex.seed
+        .run()
+        .then(() => startServer())
+        .catch(console.log);
+    })
+    .catch(console.log);
+} else {
+  startServer();
+}
