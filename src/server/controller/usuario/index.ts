125e3e802877b8633916a63fd36d6ca547895ba2
Luciano Sabino <lb255043@gmail.com>
2025-01-20 15:32:26 -0300

    feat: add getAll de usuario

--- src/server/controller/usuario/index.ts
+++ src/server/controller/usuario/index.ts
@@ -1,7 +1,11 @@
 import * as signIn from "./SignIn";
 import * as signUp from "./SignUp";
+import * as getAllUsuario from "./GetAll";
+import * as update from "./Update";
 
 export const UsuarioController = {
-    ...signIn,
-    ...signUp,
+  ...signIn,
+  ...signUp,
+  ...getAllUsuario,
+  ...update,
 };
