f98a01939328a2d83de912a534b6e9c83d60ba63
Luciano Sabino <lb255043@gmail.com>
2025-03-02 21:20:28 -0300

    atualização

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,40 @@
 import { Knex } from "./server/database/Knex";
 import { app } from "./server/server";
 
-const startServer = () => {
-  app.listen(process.env.PORT || 8080, () => {
-    console.log(`App rodando na porta ${process.env.PORT || 8080}`);
-  });
-};
+// const startServer = () => {
+//   app.listen(process.env.PORT || 8080, () => {
+//     console.log(`App rodando na porta ${process.env.PORT || 8080}`);
+//   });
+// };
 
-const runMigrationsIfNeeded = async () => {
-  try {
-    console.log("Iniciando migrações...");
-    await Knex.migrate.latest(); // Executa as migrações mais recentes no banco de dados PostgreSQL na nuvem
-    console.log("Migrações concluídas. Iniciando o servidor...");
-    startServer();
-  } catch (error: any) {
-    console.error("Erro ao verificar migrações:", error);
-    process.exit(1);
-  }
-};
+// const runMigrationsIfNeeded = async () => {
+//   try {
+//     console.log("Iniciando migrações...");
+//     await Knex.migrate.latest(); // Executa as migrações mais recentes no banco de dados PostgreSQL na nuvem
+//     console.log("Migrações concluídas. Iniciando o servidor...");
+//     startServer();
+//   } catch (error: any) {
+//     console.error("Erro ao verificar migrações:", error);
+//     process.exit(1);
+//   }
+// };
 
-// Verificando se estamos no ambiente de produção ou de desenvolvimento
-if (process.env.IS_LOCALHOST !== "true") {
-  runMigrationsIfNeeded(); // Verifica e roda as migrações se necessário
-} else {
-  startServer(); // Inicia o servidor direto no ambiente local
+// // Verificando se estamos no ambiente de produção ou de desenvolvimento
+// if (process.env.IS_LOCALHOST !== "true") {
+//   runMigrationsIfNeeded(); // Verifica e roda as migrações se necessário
+// } else {
+//   startServer(); // Inicia o servidor direto no ambiente local
+// }
+
+console.log("Iniciando migrações...");
+async function main() {
+  await Knex.migrate.latest(); // Executa as migrações mais recentes no banco de dados
 }
+
+main().catch(console.error);
+
+console.log("Migrações concluídas. Iniciando o servidor...");
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log("Api rodado na porta 8080");
+});
